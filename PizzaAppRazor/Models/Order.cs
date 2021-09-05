c1a4d6ef0b7ed1721090a993d7b3d06f33fe2541
YomnaTarek <yomnaragab13@gmail.com>
2021-09-05 16:21:01 +0200

    Added GET request and UI

---
+++ PizzaAppRazor/Models/Order.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+
+namespace PizzaAppRazor.Models
+{
+    public class Order
+    {
+        private const float _taxes = 15.0f;
+        private static int _currentId = 0;
+        public float TotalPrice { get; set; }
+        public int NumOfPizzas { get; set; }
+        public int UserId
+        {
+            get
+            {
+                return _currentId;
+            }
+        }
+        public List<Pizza> ListOfPizzas { get; set; }
+        public Order()
+        {
+            _currentId++;
+        }
+        public bool AddPizza(Pizza tobeAddedPizza)
+        {
+            try
+            {
+                ListOfPizzas.Add(tobeAddedPizza);
+                return true;
+            }
+            catch (KeyNotFoundException)
+            {
+                return false;
+            }
+        }
+        public float OrderPrice()
+        {
+            foreach (Pizza item in ListOfPizzas)
+            {
+                TotalPrice += item.CalculatePrice();
+            }
+            TotalPrice += _taxes;
+            return this.TotalPrice;
+        }
+    }
+}
---
+++ PizzaAppRazor/Pages/OrderForm.cshtml.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaAppRazor.Models;
+using PizzaAppRazor.Pages;
+using Newtonsoft.Json;
+ 
+namespace PizzaAppRazor.Pages
+{
+    public class OrderFormModel : PageModel
+    {
+        private readonly IHttpClientFactory _clientFactory;
+        public PizzaModel PizzaMenu { get; set; }
+        [BindProperty]
+        public Order PrefOrder { get; set; }
+        [BindProperty]
+        public Pizza SinglePizza { get; set; }
+        public OrderFormModel(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task OnGet()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "/components");
+            var client = _clientFactory.CreateClient("PizzaAppApi");
+            var response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                var serializedResponse = await response.Content.ReadAsStringAsync();
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+                PizzaMenu = System.Text.Json.JsonSerializer.Deserialize<PizzaModel>(serializedResponse, options);
+            }
+        }
+
+        public async Task <IActionResult> OnPost()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, "/createPizza");
+            request.Headers.Add("Accept", "application/json");
+            request.Content = new StringContent(JsonConvert.SerializeObject(PrefOrder), System.Text.Encoding.UTF8, "application/json");
+            var client = _clientFactory.CreateClient("PizzaAppApi");
+            var response = await client.SendAsync(request);
+            return Redirect("/Index");
+        }
+    }
+
+}
